fa75d8ab115307ae519d486ad01fdab687cb015d
fibletype <fibletyp@gmail.com>
2020-10-25 16:13:53 +0300

    add summon function

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+# n = number of robots
+# width = width of corridor
+
+n = 150
+x = 100
+width = 1000
+
+robots = []
+ 
+
 class robot:
     def __init__(self, x, y):
         self.x = x
@@ -5,3 +15,12 @@ class robot:
         self.v = 0
         self.wait = 0
         self.currentStatus = -1
+
+def summonRobots(n, width, robots):
+    for i in range(n):
+        robots.append(robot(x, i * width / (n + 2)))
+    return robots
+
+
+robotsBuf = summonRobots(n, width, robots)
+print(robotsBuf)
