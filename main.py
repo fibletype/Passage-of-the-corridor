1a9d702eff59c6a2002fbe10170adff0b3d88b45
Vadim <darksteellord@gmail.com>
2020-10-25 16:36:04 +0300

    fixes

--- main.py
+++ main.py
@@ -1,26 +1,25 @@
-# n = number of robots
-# width = width of corridor
+"""Robot module
 
-n = 150
-x = 100
-width = 1000
+n = number of robots
+width = width of corridor
+"""
 
-robots = []
- 
-
-class robot:
-    def __init__(self, x, y):
+class Robot:
+    def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
         self.v = 0
         self.wait = 0
-        self.currentStatus = -1
+        self.current_status = -1
 
-def summonRobots(n, width, robots):
-    for i in range(n):
-        robots.append(robot(x, i * width / (n + 2)))
-    return robots
+    def __repr__(self) -> str:
+        return f"robot: x={self.x}, y={self.y}, v={self.v}, " +\
+            f"wait={self.wait}, current_status={self.current_status}"
 
-
-robotsBuf = summonRobots(n, width, robots)
-print(robotsBuf)
+            
+if __name__ == "__main__":
+    n = 150
+    x = 100
+    width = 1000
+    robotsBuf = [Robot(x, i * width / (n + 2)) for i in range(n)]
+    print(*robotsBuf, sep="\n")
